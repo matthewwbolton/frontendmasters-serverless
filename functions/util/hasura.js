1269781b7aa92e37ffd445ffa0d3f2b0996364a5
Matthew Bolton <matthewwbolton@gmail.com>
2021-01-05 18:25:37 -0500

    created util function hasura.js to retrieve data from hasura database

---
+++ functions/util/hasura.js
@@ -0,0 +1,18 @@
+const fetch = require("node-fetch");
+
+async function query({ query, variables = {} }) {
+  const result = await fetch(process.env.HASURA_API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Hasura-Admin-Secret": process.env.HASURA_ADMIN_SECRET,
+    },
+    body: JSON.stringify({ query, variables }),
+  })
+    .then((res) => res.json())
+    .catch((err) => console.error(err));
+
+  return result.data;
+}
+
+exports.query = query;
