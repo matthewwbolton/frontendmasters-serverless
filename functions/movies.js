daa5895e5fc6a0e400520b07dad04437481ae4f0
Matthew Bolton <matthewwbolton@gmail.com>
2021-01-05 17:06:45 -0500

    added code to movies.js to retrieve data from omdb api

--- functions/movies.js
+++ functions/movies.js
@@ -1,8 +1,33 @@
+const { URL } = require("url");
+const fetch = require("node-fetch");
 const movies = require("../data/movies.json");
 
 exports.handler = async () => {
+  const api = new URL("https://www.omdbapi.com/");
+
+  // Add the secret API key
+  api.searchParams.set("apikey", process.env.OMDB_API_KEY);
+
+  const promises = movies.map((movie) => {
+    //use the movie's IMDB ID to look up details
+    api.searchParams.set("i", movie.id);
+
+    return fetch(api)
+      .then((res) => res.json())
+      .then((data) => {
+        const scores = data.Ratings;
+
+        return {
+          ...movie,
+          scores,
+        };
+      });
+  });
+
+  const moviesWithRatings = await Promise.all(promises);
+
   return {
     statusCode: 200,
-    body: JSON.stringify(movies),
+    body: JSON.stringify(moviesWithRatings),
   };
 };
